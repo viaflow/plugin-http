f6c40cf3b9a3a466a8a510b22df1e2b5cd115f26
Sirius Guo <guotuo1024@gmail.com>
2018-08-09 15:44:40 +0800

    finish http module

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-import request from 'request';
+import request from 'request-promise';
 
-
-
-export const Init = (input) => {
-
-}
-
-export const Exec = () => {
-
-}
-
-export const Display = {
+export const Define = {
+    name: "HTTP",
     fields: [
         {
-            
+            name: 'method',
+            display: 'Method',
+            required: true,
+            type: String
+        }, {
+            name: 'uri',
+            display: 'URI',
+            required: true,
+            type: String
+        }, {
+            name: 'qs',
+            display: 'Query String',
+            type: Object,
+        }, {
+            name: 'headers',
+            display: 'Headers',
+            type: Object
+        }, {
+            name: 'body',
+            display: 'Body',
+            type: Object
+        }, {
+            name: 'extends',
+            display: 'Extends',
+            type: Object
         }
     ]
+}
+
+export const Execute = async input => {
+    let options = {
+        method: input.data.method,
+        uri: input.data.uri,
+        qs: input.data.qs,
+        headers: input.data.headers,
+        body: input.data.body
+    };
+    options = Object.assign(options, input.data.extends);
+    return await request(options);
 }
